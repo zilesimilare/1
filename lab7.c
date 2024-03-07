6282b718a63313c47c62d7c952a2c8781d244250
Bianca Ciobanu <biancaciob@gmail.com>
2024-03-07 13:23:10 +0200

    recursivitate

--- lab7.c
+++ lab7.c
@@ -1,20 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int function(int x){
-	x=x-2;
-	x=function(x);
-	return x;
+int function(int x)
+{
+
+    if(x > 0)
+    {
+        x=x-2;
+        x=function(x);
+    }
+
+    return x;
 }
 
-int main(){
-	int a=30, i=0;
-	
-	while (a>0){
-		a=function(a);
-		i++;
-	}
-	printf("%d", i);
-	return 0;	
+int main()
+{
+    int a=30, i=0;
+
+    while (a>0)
+    {
+        a=function(a);
+        i++;
+    }
+    printf("%d", i);
+    return 0;
 }
 
